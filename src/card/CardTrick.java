e26835d1004ca674224ba422433ad94643d498a0
komalgill123 <komalp44@sheridancollege.ca>
2025-05-27 11:58:28 -0400

    step3

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,24 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
+import java.util.Random;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
+ * Modified: Komalpreet Kaur
+ * Student id: 991797098
+ * Date: 27 May,2025
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        Random rand = new Random();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue(rand.nextInt(13) + 1);             // random value 1-13
+            c.setSuit(Card.SUITS[rand.nextInt(Card.SUITS.length)]);  // random suit
+            magicHand[i] = c;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            for (Card card : magicHand) {
+                System.out.println(card);
+        }
         }
         
         //insert code to ask the user for Card value and suit, create their card
